934267f9712491919596616aaf771878c8d36086
tt5083 <pcdiyamd@hotmail.com>
2026-01-07 14:43:37 +0800

    更改標題名稱

--- calendar_js.php
+++ calendar_js.php
@@ -13,7 +13,7 @@ $csrf_token = $_SESSION['csrf_token'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="<?php echo $csrf_token; ?>">
-    <title>活動日曆 - 原生 JS 版</title>
+    <title>活動日曆</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/appcsslib.css" rel="stylesheet">
